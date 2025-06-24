2a280a7dcd9a0e470edf50c73729b6f04cbfd165
Sumit Kumar Panika <sumitkp153@gmail.com>
2025-06-24 21:25:30 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ connectToDb();
 app.use(cookieParser());
 // Fix CORS to allow credentials and set allowed origin
 app.use(cors({
-  origin: 'https://excel-analytics-frontend.onrender.com',
+  origin: 'https://excel-analytics-2cbb.onrender.com',
   credentials: true
 }));
 
