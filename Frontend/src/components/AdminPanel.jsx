cfbad6d884b818f466e2bb9f61f14e75a1d8d30b
Sumit Kumar Panika <sumitkp153@gmail.com>
2025-06-24 19:45:58 +0530

    Update AdminPanel.jsx

--- Frontend/src/components/AdminPanel.jsx
+++ Frontend/src/components/AdminPanel.jsx
@@ -8,7 +8,7 @@ function AdminPanel() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
-  const url = `https://excelanalytics-backend.onrender.com`;
+  const url = `https://excel-analytics-backend-qmt1.onrender.com`;
  
 
   useEffect(() => {
